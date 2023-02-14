6dd08caf0350636c79ce4ad0a72f2f9fb05055ed
Velliton <evg92faut@gmail.com>
2023-02-14 23:45:44 +0700

    popular section

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.main-slider').slick({
+        arrows: true,
+        dots: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        adaptiveHeight:true,        
+    });
+
+})
+
+$(document).ready(function() {
+    $('.slider2').slick({
+        arrows: true,
+        dots: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+    });
+
+})
